a35c9ae20e8363589fbf42e8ccf4c966a6056e64
Unfunnyes <awdmeres@gmail.com>
2025-08-05 19:34:31 -0300

    actualizando

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+    let amigo = (document.querySelector('#amigo').value);
+    amigos.push(amigo);
+    limpiarCaja();
+        if (amigo === "");
+            alert('Por favor, inserte un nombre.');
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
